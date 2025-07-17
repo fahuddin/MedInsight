07fd62d59f255c6f44f2cafc41fb43c5dc98f233
fahuddin <fahuddin@bu.edu>
2025-07-17 11:31:56 -0400

    added text_cleaning

--- src/nlp/text_cleaning.py
+++ src/nlp/text_cleaning.py
@@ -8,6 +8,6 @@ def clean_text(text):
     text = text.lower().strip()
     return text
 
-def lemmatize(text) -> str:
+def lemmatize(text):
     doc = nlp(text)
     return " ".join([token.lemma_ for token in doc if not token.is_stop and token.is_alpha])
