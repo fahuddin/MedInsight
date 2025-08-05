0aa0e38adefedabaecf1f8f408b86fddc9247bb8
fahuddin <fahuddin@bu.edu>
2025-08-05 00:28:22 -0400

    removed clinical_notes

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def feature_engineering(df):
-    df = df.drop(columns=["patient_id", "clinical_notes"], errors="ignore")
+    df = df.drop(columns=["patient_id"], errors="ignore")
     
     # One-hot encode gender and smoker
     df = pd.get_dummies(df, columns=["gender", "smoker"], drop_first=True)
