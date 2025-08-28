1cc6234de6c92a6cae2a4ad1a8dea5190ba72ca5
fahuddin <fahuddin@bu.edu>
2025-08-28 02:15:14 -0400

    edited train_and_save_model fun

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,10 +2,11 @@
 
 import pandas as pd
 import joblib
+import os
 from xgboost import XGBClassifier
 from features.feature_engineering import feature_engineering
 
-def train_and_save_model(data_path: str, model_path: str):
+def train_and_save_model(data_path: str):
     df = pd.read_csv(data_path)
     df = df.dropna()
 
@@ -14,6 +15,9 @@ def train_and_save_model(data_path: str, model_path: str):
 
     model = XGBClassifier(n_estimators=100, max_depth=5, use_label_encoder=False, eval_metric='logloss')
     model.fit(X, y)
+    model_path = "models/xgb_readmission_model.pkl"
+
+    os.makedir(os.path.dirname(model_path), exist_ok=True)
 
     joblib.dump(model, model_path)
     
